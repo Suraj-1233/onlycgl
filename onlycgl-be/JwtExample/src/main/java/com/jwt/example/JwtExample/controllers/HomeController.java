deb496f639c717fca4282b0667f9ef6ae4882f65
Suraj Kannujiya <surajkannujiya517@gmail.com>
2024-08-04 20:24:29 +0530

    backend

---
+++ onlycgl-be/JwtExample/src/main/java/com/jwt/example/JwtExample/controllers/HomeController.java
@@ -0,0 +1,32 @@
+package com.jwt.example.JwtExample.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jwt.example.JwtExample.models.User;
+import com.jwt.example.JwtExample.services.UserService;
+
+@RestController
+@RequestMapping("/home")
+public class HomeController {
+
+    
+// htttp://localhost:8081/home/user
+  @Autowired
+  private UserService userService;
+
+
+ @GetMapping("/users")
+ public List<User> getUser(){
+
+    System.err.println(" gettting Users");
+// return this.userService.getUsers();
+ return this.userService.getUsers();
+ }
+
+
+}
---
+++ onlycgl-be/JwtExample/src/main/java/com/jwt/example/JwtExample/services/UserService.java
@@ -0,0 +1,29 @@
+package com.jwt.example.JwtExample.services;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.stereotype.Service;
+
+import com.jwt.example.JwtExample.models.User;
+ @Service
+public class UserService {
+     private List<User> store=new ArrayList<>();
+    
+
+
+      public UserService() {
+          
+          store.add(new User(UUID.randomUUID().toString(),"suraj","suraj@123gmail.com"));
+          store.add(new User(UUID.randomUUID().toString(),"suraj1","suraj@1233gmail.com"));
+
+          store.add(new User(UUID.randomUUID().toString(),"suraj2" ,"suraj@1234@gmail.com"));
+
+      }
+      public  List<User> getUsers(){
+          return this.store;
+
+      }
+
+}
